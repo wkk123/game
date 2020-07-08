db464e2105bc2bb4367f150cf70e3641dbf62796
wkk <1754393348@qq.com>
2020-07-08 09:45:43 +0800

    2020.07.08

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,12 +7,6 @@ Page({
     gradeArr:['简单-单色','中级-双色','高级-四色'],
     gradeIndex: 0,
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   // 选择简单程度
   btnItem: function(e){
     let index = e.currentTarget.dataset.idx;
@@ -20,9 +14,11 @@ Page({
       gradeIndex: index
     })
   },
+  // 选择关卡 开始游戏
   gradeBtn: function(e){
-    let index = e.currentTarget.dataset.index;
-    console.log('index',index)
+    wx.navigateTo({
+      url: '../poker/poker'+`?customsPass=${e.currentTarget.dataset.index}`
+    })
   },
   onLoad: function () {
 
