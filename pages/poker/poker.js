5197372194eacd60c6f051c48a2610d237767332
wkk <1754393348@qq.com>
2020-09-03 15:43:45 +0800

    2020.09.03

--- pages/poker/poker.js
+++ pages/poker/poker.js
@@ -5,15 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    pokerData:[
+      ['c', 'c', 'c', 'c'],
+      ['c', 'd', 'c', 'd'],
+      ['a', 'b', 'c', 'd']
+    ],
+    pokerType:[],//当前类型
+    pokerAllPet:[],//扑克数组
+    pokerDoneGroup:[],//首发牌数组
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('options',options)
-    console.log('options',options.customsPass)
+    const { customsPass } = options;
+    const { pokerData } = this.data;
+    const pokerType = pokerData[Number(customsPass) - 1];
+    this.setData({
+      pokerType: pokerType
+    },()=>{
+      this.initGame()
+    })
   },
 
   /**
@@ -63,5 +76,37 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 初始化游戏
+  initGame:function(){
+    const { pokerType } = this.data;
+    console.log('pokerType', pokerType)
+    this.initRandomPoker()
+  },
+  // 初始化扑克
+  initRandomPoker(){
+    const arr = [];
+    const { pokerType } = this.data;
+    for (let index = 0; index < pokerType.length; index++) {
+      for (let it = 0; it < 13; it++) {
+        let a = `${pokerType[index]}-${it+1}`; 
+        arr.push(a,a)
+      }
+    }
+    this.setData({
+      pokerAllPet: this.shuffle(arr)
+    })
+  },
+  // 数组 随机
+  shuffle:function(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+      let idx = Math.floor(Math.random() * (len - i));
+      let temp = arr[idx];
+      arr[idx] = arr[len - i - 1];
+      arr[len - i - 1] = temp;
+    }
+    return arr;
   }
+
 })
